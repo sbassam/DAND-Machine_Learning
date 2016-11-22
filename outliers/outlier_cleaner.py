7388fe39bbd6a3378bdf028485573bb7f95d5fc2
Soroush Bassam <sbassam@crp.org>
2016-11-22 17:42:50 -0500

    outlier

---
+++ outliers/outlier_cleaner.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+
+def outlierCleaner(predictions, ages, net_worths):
+    """
+        Clean away the 10% of points that have the largest
+        residual errors (difference between the prediction
+        and the actual net worth).
+
+        Return a list of tuples named cleaned_data where 
+        each tuple is of the form (age, net_worth, error).
+    """
+    
+    cleaned_data = []
+
+    #error = abs(predictions - net_worths)
+    #print 'salam'
+
+    #print error
+    tup = ()
+    ### your code goes here
+    for i in range(len(predictions)):
+        print 'salam'
+        error = abs(net_worth[i] - predictions[i])
+        tup = ages[i], net_worth[i], error
+        cleaned_data.append(tup)
+        print tup
+        tup = ()
+
+
+    
+    return cleaned_data
